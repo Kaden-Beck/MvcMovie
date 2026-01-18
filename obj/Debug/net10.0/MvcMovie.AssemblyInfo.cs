5a5c2a5a2d19be357b2ac58a7037bb9286d2cd7e
Kaden-Beck <kadenb12@gmail.com>
2026-01-18 16:14:04 -0700

    Part 7 - Add Search

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126afa416f84c03dfe6e658f1fe52a6c3eec0c9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c9a6b984c517d031c72d566bc0eb5ee44541aee")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
