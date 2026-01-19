eec34321724302042a9698b78d35bb0a2fdb3fcf
Kaden-Beck <kadenb12@gmail.com>
2026-01-18 17:05:35 -0700

    Changed titles, added year filter

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a5c2a5a2d19be357b2ac58a7037bb9286d2cd7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83d07f6a53382112b46e0a2898f7f15c381b3f65")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
